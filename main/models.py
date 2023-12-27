7bf72413fa52d62e91022fad70f2a395ecbde84e
tim <artem.m2013@gmail.com>
2023-12-27 18:00:19 +0300

    add models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Files(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='Пользователь')
+
+    file = models.FileField(upload_to='files/', verbose_name='Файл')
+    accept_file = models.BooleanField(default=False, verbose_name='Принятый файл')
+    denied_file = models.BooleanField(default=False, verbose_name='Отклонённый файл')
+
+    def __str__(self):
+        return f'Файл {self.user}: Принят - {self.accept_file} Отклонён - {self.denied_file}'
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'

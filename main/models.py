04e4725c269934566d8ccc57971b1b8a41df154d
tim <artem.m2013@gmail.com>
2023-12-28 20:57:57 +0300

    change model

--- main/models.py
+++ main/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.conf import settings
 
+FILE_STATUS = [(1, 'Принято'), (2, 'Отклонено'), (3, 'На проверке')]
+
 
 class Files(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='Пользователь')
 
     file = models.FileField(upload_to='files/', verbose_name='Файл')
-    accept_file = models.BooleanField(default=False, verbose_name='Принятый файл')
-    denied_file = models.BooleanField(default=False, verbose_name='Отклонённый файл')
+    status = models.PositiveSmallIntegerField(choices=FILE_STATUS, default=3)
 
     def __str__(self):
-        return f'Файл {self.user}: Принят - {self.accept_file} Отклонён - {self.denied_file}'
+        return f'Файл {self.user}: Статус {self.status}'
 
     class Meta:
         verbose_name = 'Файл'
